9321e4567a8cc08d07139048e6b507c77ff97cb7
Joshua Smith <mtgmirage@protonmail.com>
2024-12-31 14:04:41 -0600

    implemented CORS for https integration

--- Program.cs
+++ Program.cs
@@ -99,12 +99,23 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add CORS
+builder.Services.AddCors();
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.UseCors(options => 
+        {
+            options.AllowAnyOrigin();
+            options.AllowAnyMethod();
+            options.AllowAnyHeader();
+        });
 }
 app.UseHttpsRedirection();
 //////////////////////////////////////
